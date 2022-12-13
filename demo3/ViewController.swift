1f893a2c45fac7adb87314e1c0ef4911fd820017
ace307 <florent.ace307@gmail.com>
2022-12-13 16:37:21 +0800

    繼承作業練習

--- demo3/ViewController.swift
+++ demo3/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var msgLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //父類別
+    class Animal{
+        var skill :String = ""
+        func speak(name:String) -> String{
+            return "My name is \(name)"
+        }
+    }
+    //子類別
+    class Cat : Animal{
+        override func speak(name: String) -> String {
+            return "My name is \(name),and I can \(skill)"
+        }
+    }
 
-
+    class Dog : Animal{
+        override init() {
+            super.init()//調用父類的初始化器
+            super.skill = "chasing my tail"
+        }
+        
+    
+    }
+    
+    //按鈕事件
+    @IBAction func catSpeak(_ sender: Any) {
+        
+        let yellowCat = Cat()
+        yellowCat.skill = "jump high"
+        let str = yellowCat.speak(name: "小山")
+        
+        msgLabel.text = str
+        
+    }
+    
+    
+    @IBAction func dogSpeak(_ sender: Any) {
+        let whiteDog = Dog()
+        
+        msgLabel.text = whiteDog.speak(name: "小吉")+"and I love "+whiteDog.skill
+    }
+    
 }
 
